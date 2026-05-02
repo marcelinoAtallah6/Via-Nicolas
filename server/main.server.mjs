4d6342c33f55cb4bd02b18fb855d024857b125f0
Marcelino Atallah <marcelino_atallah@hotmail.com>
2026-05-02 18:48:37 +0300

    Auto-generated commit

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-PXYARICT.mjs";import"./chunk-5T2ZEFGP.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-6EMIHCF7.mjs";import"./chunk-5T2ZEFGP.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
